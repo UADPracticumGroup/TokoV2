03dde571532a44e0c02be7770cf4b08b8bc6c9b9
Ghofar <ghofarsuprapto@gmail.com>
2022-04-08 16:30:29 +0700

    Create Harga.h

---
+++ library/Harga.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const int HargaAyamGeprek = 21000;
+const int HargaAyamGoreng = 17000;
+const int HargaUdangGoreng = 19000;
+const int HargaCumiGoreng = 20000;
+const int HargaAyamBakar = 25000;
